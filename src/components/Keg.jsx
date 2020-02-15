f4657608712d6e344e45956f3790a5567ad9b8ae
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-15 13:09:30 -0800

    list in table beers from kegList in OnTap component

---
+++ src/components/Keg.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props){
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.brand}</td>
+      <td>{props.type}</td>
+      <td>{props.price}</td>
+      <td>{props.alcoholContent}</td>
+    </tr>
+  );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  alcoholContent: PropTypes.number.isRequired
+}
+
+export default Keg;
--- src/components/onTap.jsx
+++ src/components/onTap.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import kegList from '../data/kegList';
+import Keg from './Keg';
 
 function OnTap() {
   return(
     <div>
       <p>This is OnTap</p>
-      {kegList.map((keg, index) =>
-        <Keg
-          name={keg.name}
-          brand={keg.brand}
-          type={keg.type}
-          price={keg.price}
-          alcoholContent={keg.alcoholContent}
-        />
-      )}
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Brand</th>
+          <th>Type</th>
+          <th>Price</th>
+          <th>Alcohol Content</th>
+        </tr>
+        {kegList.map((keg, index) =>
+          <Keg
+            name={keg.name}
+            brand={keg.brand}
+            type={keg.type}
+            price={keg.price}
+            alcoholContent={keg.alcoholContent}
+          />
+        )}
+      </table>
     </div>
   );
 }
