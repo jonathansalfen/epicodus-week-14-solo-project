cb0d768013f1a8dac8dda64c6361124aae440810
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-15 10:26:17 -0800

    add scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style/App.css';
+import './style/App.scss';
 import { Switch, Route } from 'react-router-dom';
 import About from './components/About.jsx';
 import Header from './components/Header';
