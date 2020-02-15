4d4f244483cdfcadb798f77a722b055e47737976
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-14 21:10:11 -0800

    add react router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+wassup
+    </div>
+  );
+}
+
+export default App;
