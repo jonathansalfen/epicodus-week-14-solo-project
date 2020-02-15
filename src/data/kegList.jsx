d58a1d06d87ee3fd837a669c3b8ded8a27eddb24
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-15 14:29:49 -0800

    increase price

--- src/data/kegList.jsx
+++ src/data/kegList.jsx
@@ -2,48 +2,48 @@ export default [
   {
     name: 'Orange Dream',
     brand: 'Sutherland',
-    type: 'pilsner',
-    price: 45.78,
+    type: 'Pilsner',
+    price: 145.78,
     alcoholContent: 6.7,
     pintsAvailable: 124
   },
   {
     name: 'IPA Crush',
     brand: 'Colorado Land',
-    type: 'ipa',
-    price: 66.55,
+    type: 'IPA',
+    price: 166.55,
     alcoholContent: 8.9,
     pintsAvailable: 124
   },
   {
     name: 'Chocorama',
     brand: 'Bend Brewery',
-    type: 'stout',
-    price: 56.76,
+    type: 'Stout',
+    price: 156.76,
     alcoholContent: 5.5,
     pintsAvailable: 60
   },
   {
     name: 'James and the Giant Peach Flavored Beer',
     brand: 'Nor Cal Distillery',
-    type: 'pale ale',
-    price: 78.90,
+    type: 'Pale Ale',
+    price: 178.91,
     alcoholContent: 4.5,
     pintsAvailable: 124
   },
   {
     name: 'Zesty Tomato Lite',
     brand: 'Portland Beer Company',
-    type: 'light',
-    price: 56.93,
+    type: 'Light',
+    price: 156.93,
     alcoholContent: 5.7,
     pintsAvailable: 30
   },
   {
     name: 'Yummy Yum',
     brand: 'The Mountain Beer Co.',
-    type: 'lager',
-    price: 45.90,
+    type: 'Lager',
+    price: 145.99,
     alcoholContent: 4.3,
     pintsAvailable: 5
   },
