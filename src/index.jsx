2422eadccf59d486829dd92a1a2bdef6c2fe874f
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-15 15:51:59 -0800

    change images location, create assests folder

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
   </HashRouter>, document.getElementById('root'));
 
