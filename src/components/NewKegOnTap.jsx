d4268b8f22ac39545590c7f63882dd52eb09d3dc
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-15 14:49:37 -0800

    add component

---
+++ src/components/NewKegOnTap.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function NewKegOnTap() {
+  return(
+    <div>
+      <form>
+        <input
+          type='text'
+          id='name'
+          placeholder='Beer Name'
+        />
+        <input
+          type='text'
+          id='brand'
+          placeholder='Brewery Brand'
+        />
+        <input
+          type='text'
+          id='type'
+          placeholder='Brew Type'
+        />
+        <input
+          type='integer'
+          id='price'
+          placeholder='Keg Price'
+        />
+        <input
+          type='integer'
+          id='alcoholContent'
+          placeholder='Alcohol Content'
+        />
+        <input
+          type='integer'
+          id='pintsAvailable'
+          placeholder='Total Pints in Keg'
+        />
+        <input
+          type='integer'
+          id='tapLocation'
+          placeholder='Tap Location'
+        />
+      </form>
+    </div>
+  );
+}
+
+export default NewKegOnTap;
